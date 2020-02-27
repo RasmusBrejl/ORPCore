7c9eef550184317c32b0402aa5934ee79b0768a1
madc <madc@netcompany.com>
2020-02-28 00:36:52 +0100

    migration and frontend

--- ORPCore/ClientApp/src/app/user/user-login/user-login.component.ts
+++ ORPCore/ClientApp/src/app/user/user-login/user-login.component.ts
@@ -2,33 +2,31 @@ import { Component } from '@angular/core';
 import { User } from '../user';
 import { UserService } from '../User.Service';
 
+
 @Component({
   selector: 'user-login-component',
   templateUrl: 'user-login.component.html',
   styleUrls: ['user-login.component.css']
 })
 export class UserLoginComponent {
+  
+  public constructor (private userService: UserService) {
 
- //private userService: UserService;
-
- public constructor() {
-    
- }
+  }
   public user: User;
   public loginfailed: boolean;
 
   public ngOnInit() {
-    this.user = new User();
+    this.user = new User(); 
     this.user.name = "1";
   }
 
   public login(){
     
-    this.loginfailed = !this.loginfailed;
-    // var userLogin = this.userService.tryLogIn(this.user);
-    // if(!userLogin) {
-    //   this.loginfailed = true;
-    // }
+    //this.loginfailed = !this.loginfailed;
+    var userLogin = this.userService.tryLogIn(this.user);
+    if(!userLogin) {
+      this.loginfailed = true;
+    }
   }
-
 }
---
+++ ORPCore/Migrations/20200227233610_ThirdMigration.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ORPCore.Migrations
+{
+    public partial class ThirdMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Connections_Cities_CityOneCityId",
+                table: "Connections");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Connections_Cities_CityTwoCityId",
+                table: "Connections");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Connections_CityOneCityId",
+                table: "Connections");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Connections_CityTwoCityId",
+                table: "Connections");
+
+            migrationBuilder.DropColumn(
+                name: "CityOneCityId",
+                table: "Connections");
+
+            migrationBuilder.DropColumn(
+                name: "CityTwoCityId",
+                table: "Connections");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CityOne",
+                table: "Connections",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CityTwo",
+                table: "Connections",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CityOne",
+                table: "Connections");
+
+            migrationBuilder.DropColumn(
+                name: "CityTwo",
+                table: "Connections");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CityOneCityId",
+                table: "Connections",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CityTwoCityId",
+                table: "Connections",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Connections_CityOneCityId",
+                table: "Connections",
+                column: "CityOneCityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Connections_CityTwoCityId",
+                table: "Connections",
+                column: "CityTwoCityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Connections_Cities_CityOneCityId",
+                table: "Connections",
+                column: "CityOneCityId",
+                principalTable: "Cities",
+                principalColumn: "CityId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Connections_Cities_CityTwoCityId",
+                table: "Connections",
+                column: "CityTwoCityId",
+                principalTable: "Cities",
+                principalColumn: "CityId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
