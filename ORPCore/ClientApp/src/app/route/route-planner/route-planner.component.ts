65d08a6cc29ebc42d4300af16e4d7a7346b01cce
nc-ritr <ritr@netcompany.com>
2020-02-28 05:25:36 +0100

    Added content

--- ORPCore/ClientApp/src/app/route/route-planner/route-planner.component.ts
+++ ORPCore/ClientApp/src/app/route/route-planner/route-planner.component.ts
@@ -1,1 +1,35 @@
-﻿
+import { Component } from '@angular/core';
+//import { RouteService } from '../Route.Service';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'route-planner-component',
+  templateUrl: 'route-planner.component.html',
+  styleUrls: ['route-planner.component.css']
+})
+export class RoutePlannerComponent {
+
+  public destinations: Array<string>;
+  public destination: string;
+  public weight: string;
+  public height: string;
+  public depth: string;
+  public width: string;
+
+  public ngOnInit() {
+
+    this.weight = "";
+    this.height = "";
+    this.width = "";
+    this.depth = "";
+    this.destinations = new Array<string>();
+    this.destinations.push("Whale City", "Congo", "Africa");
+
+  }
+
+  //private userService: UserService;
+
+  //  public constructor(private routeService: RouteService) {
+
+  //  }
+}
